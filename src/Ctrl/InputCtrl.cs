0771efb21802467be20af93fc8c1b13af77bbe6c
youzi151 <youzi151@gmail.com>
2016-01-15 01:03:09 +0800

    新增 InputCtrl部分實作、InputUtil

--- src/Ctrl/InputCtrl.cs
+++ src/Ctrl/InputCtrl.cs
@@ -6,7 +6,7 @@ public class InputCtrl{
 	/*==============Members===============*/
 
 	//正在偵聽操作的物件清單
-	public List<T> listenerList;
+	public List<Action<T>> listenerList;
 
 
 	/*=============Components=============*/
@@ -17,12 +17,33 @@ public class InputCtrl{
 
 	/*===========Public Function==========*/
 	//加入偵聽者  params: 偵聽者本身, 優先度
-	public void addListener(T listener, int priority){}
+	public void addListener(Action<T> listener, int priority){}
 
 	//移除偵聽者  params: 偵聽者本身
-	public void removeListener(T listener){}
+	public void removeListener(Action<T> listener){}
+
+	//檢查Input，可放於Update執行
+	public void checkInput(){
+		Object info = InputUtil.getHitObj_example();
+		if (info != null){
+			this.notifyListener(info);
+		}
+	}
 
 	/*==========Private Function==========*/
+
+	//依序通知每一個listener
+	private void notifyListeners(T inputInfo){
+		//先排序，以免優先度失效
+		this.sortListener();
+
+		foreach(Action<T> eachListener in listenerList){
+			eachListener(inputInfo);
+		}
+	}
+
+	//依照優先度排序
+	private void sortListener(){}
 
 
 
---
+++ src/Util/InputUtil.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputUtil{
+
+	/*==============Members===============*/
+
+
+	/*=============Components=============*/
+
+
+	/*================Event===============*/
+
+
+	/*===========Public Function==========*/
+
+	//自定義各項Input操作
+	//建議以get當作前綴詞，因為需要return一個input的info
+
+
+	//舉例
+	public Object getHitObj_example(){
+		if (某某操作){
+			return 包裝好的Input資料;
+		}else{
+			return null;
+		}
+	}
+
+	/*==========Private Function==========*/
+
+
+}
